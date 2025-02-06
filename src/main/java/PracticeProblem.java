d74ffde1d04fdd6da18361e11e40b86ba9b32203
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-02-06 15:13:18 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
 		q1();
 		q2();
 		q3();
@@ -13,38 +14,66 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(input.nextLine().charAt(0));
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner input = new Scanner(System.in);
+		Boolean bool;
+		System.out.print("In: ");
+		bool = input.nextBoolean();
+		input.nextLine();
+		System.out.println(!bool);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(input.nextInt() > 5);
+		input.nextLine();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner input = new Scanner(System.in);
+		double fractionalDouble;
+		System.out.print("In: ");
+		fractionalDouble = input.nextDouble();
+		input.nextLine();
+		System.out.println(fractionalDouble <= 2.0 && fractionalDouble >= -2.0);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(input.nextLine().equals("Hello World"));
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		int num;
+		int num2;
+		num = input.nextInt();
+		input.nextLine();
+		System.out.print("In: ");
+		num2 = input.nextInt();
+		input.nextLine();
+		System.out.println(num <= num2);
 	}
 
 	public static void q7() {
-		//Write question 7 code here
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("In: ");
+		double fractionalDouble1;
+		double fractionalDouble2;
+		fractionalDouble1 = input.nextInt();
+		input.nextLine();
+		System.out.print("In: ");
+		fractionalDouble2 = input.nextInt();
+		input.nextLine();
+		System.out.println(fractionalDouble1 > fractionalDouble2);
 	}
 
 }
